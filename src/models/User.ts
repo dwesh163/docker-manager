bdddc9fa3ce7c9dc507a6c76c5e4d1a00658bd5e
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-02 18:21:45 +0200

    refactor: Update User model schema in User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
-const { Schema } = mongoose;
+export interface IUser extends Document {
+	name: string;
+	username?: string;
+	image?: string;
+	email: string;
+	password: string;
+	twoFactorEnabled: boolean;
+	twoFactorSecret?: string;
+}
 
-const UserSchema = new Schema({
-	provider: String,
-	name: String,
-	email: String,
-	image: String,
-	password: String,
-	username: String,
+const userSchema = new mongoose.Schema<IUser>({
+	name: { type: String, required: true },
+	username: { type: String, required: false },
+	image: { type: String, required: false },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	twoFactorEnabled: { type: Boolean, required: false, default: false },
+	twoFactorSecret: { type: String, required: false },
 });
 
-export const User = mongoose.models.User || mongoose.model('User', UserSchema);
+export const User = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
