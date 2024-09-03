b4e7135d1018ae7d02ed4f3da91fc9edf82b7253
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-03 19:20:30 +0200

    refactor: Add Sidebar component for navigation

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,14 +2,22 @@ import { ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 import { getServerSession } from 'next-auth';
+import Sidebar from '@/components/sidebar';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
 	const session = await getServerSession();
 
+	if (!session) {
+		redirect('/signin');
+	}
+
 	return (
-		<main className="flex">
-			{JSON.stringify(session)}
-			{children}
-		</main>
+		<>
+			<main className="flex">
+				<Sidebar session={session} />
+				{children}
+			</main>
+		</>
 	);
 }
