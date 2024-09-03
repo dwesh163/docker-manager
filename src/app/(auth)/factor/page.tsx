d187ebc7037449c2bc22fee67cb5a44614696b78
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-03 18:42:28 +0200

    feat: Add Two-Factor Authentication enable page component

---
+++ src/app/(auth)/factor/page.tsx
@@ -0,0 +1,18 @@
+import TwoFactorAuth from '@/components/twoFactorAuth';
+import { checkTwoFactorEnabled } from '@/lib/user';
+import { Session } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { redirect } from 'next/navigation';
+
+export default async function EnablePage() {
+	const session = await getServerSession();
+	const isTwoFactorEnabled = await checkTwoFactorEnabled(session as Session);
+
+	console.log('isTwoFactorEnabled', isTwoFactorEnabled);
+
+	if (!isTwoFactorEnabled) {
+		redirect('/enable');
+	}
+
+	return <TwoFactorAuth />;
+}
