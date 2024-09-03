dd3a3eef774c6fff64d8caaba4392f44f54f62db
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-03 20:54:42 +0200

    refactor: Update RootLayout component with Navbar and role prop

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { getServerSession } from 'next-auth';
 import Sidebar from '@/components/sidebar';
 import { redirect } from 'next/navigation';
+import Navbar from '@/components/navbar';
+import { getRole } from '@/lib/user';
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
 	const session = await getServerSession();
@@ -12,12 +14,15 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 		redirect('/signin');
 	}
 
+	const role = await getRole(session);
+
 	return (
-		<>
-			<main className="flex">
-				<Sidebar session={session} />
+		<div className="flex h-screen">
+			<Sidebar session={session} role={role} />
+			<main className="bg-muted/40 w-full">
+				<Navbar session={session} />
 				{children}
 			</main>
-		</>
+		</div>
 	);
 }
