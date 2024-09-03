42e0f71ef310c23fb5cefb71173b12886132c8b4
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-03 18:56:55 +0200

    feat: Add Google authentication provider for NextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth, { AdapterUser, NextAuthOptions, User as AuthUser, Session } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import { IUser, User } from '@/models/User';
 import db from '@/lib/mongo';
 import { isPasswordValid } from '@/lib/hash';
@@ -46,6 +47,10 @@ export const authOptions: NextAuthOptions = {
 		GitHubProvider({
 			clientId: process.env.GITHUB_ID || '',
 			clientSecret: process.env.GITHUB_SECRET || '',
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID || '',
+			clientSecret: process.env.GOOGLE_SECRET || '',
 		}),
 	],
 	session: {
