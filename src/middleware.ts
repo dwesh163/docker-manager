7a7f31e5e7667e0fc2ced2b5cf4898626cd6c988
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-03 18:41:29 +0200

    refactor: Update middleware to handle 2FA verification and redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,36 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-	const token = await getToken({ req });
+	try {
+		const token = await getToken({ req });
 
-	if (!token?.is2FACompleted && req.nextUrl.pathname !== '/enable') {
-		return NextResponse.redirect(new URL('/enable', req.url));
+		console.log('Token:', token);
+
+		const url = new URL(req.url);
+
+		if (!token?.isTwoFactorComplete && url.pathname !== '/factor' && url.pathname !== '/enable') {
+			return NextResponse.redirect(new URL('/factor', req.url));
+		}
+
+		if (token === null) {
+			return NextResponse.redirect(new URL('/signin', req.url));
+		}
+
+		return NextResponse.next();
+	} catch (error) {
+		return NextResponse.redirect(new URL('/signin', req.url));
 	}
-
-	return NextResponse.next();
 }
+
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 */
+		'/((?!api|_next/static|_next/image|signin|favicon.ico).*)',
+	],
+};
