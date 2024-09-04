26e7279a6d8676781cacd423b32e9cddd710fc91
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-04 15:05:13 +0200

    feat: Add webpack configuration for loading .node files

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
 	images: {
 		domains: ['avatars.githubusercontent.com'],
 	},
+	webpack: (config, { isServer }) => {
+		config.module.rules.push({
+			test: /\.node$/,
+			use: 'node-loader',
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
