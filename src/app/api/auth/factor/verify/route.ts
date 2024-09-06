945ce16a5f03b786e9649d9d4c1b40ff3f5e78ec
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-06 07:36:34 +0200

    refactor: Update JWT token cookie handling in 2FA verification route

--- src/app/api/auth/factor/verify/route.ts
+++ src/app/api/auth/factor/verify/route.ts
@@ -48,18 +48,19 @@ export async function POST(req: NextRequest) {
 			secret: process.env.NEXTAUTH_SECRET!,
 		});
 
+		console.log(req.headers.get('x-forwarded-proto'));
+
 		// Set the updated JWT token in the response header
 		const response = NextResponse.json({ message: '2FA verification successful' }, { status: 200 });
 		if (process.env.NODE_ENV !== 'production') {
 			response.headers.set('Set-Cookie', `next-auth.session-token=${encodedToken}; Path=/; HttpOnly; Secure; SameSite=Lax;`);
 		} else {
 			console.log('prod');
-			response.headers.set('Set-Cookie', `__Secure-next-auth.session-token=${encodedToken}; Path=/; HttpOnly; SameSite=Lax;`);
+			response.headers.set('Set-Cookie', `__Secure-next-auth.session-token=${encodedToken}; Path=/; HttpOnly; Secure; SameSite=Lax;`);
 		}
 
 		return response;
 	}
-
 	// Fallback in case token retrieval or update fails
 	return NextResponse.json({ error: 'Failed to update JWT token' }, { status: 500 });
 }
