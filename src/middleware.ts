a4a92d3e78cdbf983c9f4511a65fc013617f93da
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-07 11:46:48 +0200

    refactor: Remove console.log statements from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,6 @@ export async function middleware(req: NextRequest) {
 	try {
 		const token = await getToken({ req });
 
-		console.log('token', token);
-
 		const url = new URL(req.url);
 
 		if (token === null && url.pathname !== '/signin') {
@@ -16,17 +14,14 @@ export async function middleware(req: NextRequest) {
 		if (token?.isTwoFactorComplete && token?.TwoFactorExpiration && (token.TwoFactorExpiration as number) < Date.now()) {
 			token.isTwoFactorComplete = false;
 			token.TwoFactorExpiration = null;
-			console.log('token2', token);
 			const encodedToken = await encode({
 				token,
 				secret: process.env.NEXTAUTH_SECRET!,
 			});
 			const response = NextResponse.redirect(new URL('/', req.url));
-			console.log('env', process.env.NODE_ENV);
 			if (process.env.NODE_ENV !== 'production') {
 				response.headers.set('Set-Cookie', `next-auth.session-token=${encodedToken}; Path=/; HttpOnly; Secure; SameSite=Lax;`);
 			} else {
-				console.log('prod');
 				response.headers.set('Set-Cookie', `__Secure-next-auth.session-token=${encodedToken}; Path=/; HttpOnly; Secure; SameSite=Lax;`);
 			}
 			return response;
