bd00c29e945664cc65622926a37937165766a0f6
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-15 22:17:38 +0200

    Refactor service settings route to handle PATCH requests and update service details

---
+++ src/app/api/services/[serviceId]/settings/route.ts
@@ -0,0 +1,31 @@
+import { getServerSession, Session } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { updateService } from '@/lib/service';
+
+interface Params {
+	serviceId: string;
+}
+
+export async function PATCH(req: Request, { params }: { params: Params }) {
+	const { name, description, repository } = await req.json();
+
+	const session: Session | null = await getServerSession();
+
+	console.log('session', session);
+
+	if (!session) {
+		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+	}
+
+	if (!description && !name && !repository) {
+		return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+	}
+
+	const service = await updateService({ name, description, repository: repository !== '' ? repository : undefined, id: params.serviceId, email: session?.user?.email || '' });
+
+	if (service.error) {
+		return NextResponse.json({ error: service.error }, { status: service.status });
+	}
+
+	return NextResponse.json({ success: true });
+}
