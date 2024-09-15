b50d7935ebe2664c0b7837249e428aa5d8c50925
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-15 22:18:13 +0200

    Refactor service.ts to add url and repository fields to ServicesType and ServiceType interfaces

--- src/types/service.ts
+++ src/types/service.ts
@@ -1,5 +1,27 @@
+import { DockerType } from './docker';
 export interface ServicesType {
 	id: string;
 	name: string;
 	status: string;
+	url: string;
+	repository?: {
+		url: string;
+		image: string;
+	};
+}
+
+export interface ServiceType {
+	id: string;
+	name: string;
+	description: string;
+	owner: string;
+	status: string;
+	users: string[];
+	dockers: DockerType[];
+	url: string;
+	repository?: {
+		url: string;
+		image: string;
+	};
+	createdAt: Date;
 }
