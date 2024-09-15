c08c4eeef77a18dd288afa2cdc694a928ecd5caf
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-15 22:18:41 +0200

    Refactor service.ts to add url and repository fields to ServicesType and ServiceType interfaces

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,6 +1,6 @@
 import { IService, Service } from '@/models/Services';
 import db from './mongo';
-import { ServicesType } from '@/types/service';
+import { ServicesType, ServiceType } from '@/types/service';
 import { getRole } from './user';
 import { IUser, User } from '@/models/User';
 import { ErrorType } from '@/types/error';
@@ -10,7 +10,6 @@ export async function getServices(email: string | null | undefined): Promise<Ser
 
 	if (!email) {
 		return [];
-		throw new Error('Email is required');
 	}
 
 	const role = await getRole(email);
@@ -24,7 +23,6 @@ export async function getServices(email: string | null | undefined): Promise<Ser
 
 		if (!user) {
 			return [];
-			throw new Error('User not found');
 		}
 
 		services = await Service.find<IService>({
@@ -32,13 +30,98 @@ export async function getServices(email: string | null | undefined): Promise<Ser
 		});
 	}
 
-	return services.map((service) => {
+	const resolvedServices = await Promise.all(
+		services.map(async (service) => {
+			if (!service.repository) {
+				return {
+					id: service._id.toString(),
+					name: service.name,
+					status: service.status,
+					url: service.url,
+				};
+			}
+
+			const response = await fetch(`https://api.github.com/repos/${service.repository.replace('https://github.com/', '')}`, {
+				headers: {
+					Authorization: `token ${process.env.GITHUB_TOKEN}`,
+				},
+			});
+			const repository = await response.json();
+
+			return {
+				id: service._id.toString(),
+				name: service.name,
+				status: service.status,
+				url: service.url,
+				repository: {
+					url: service.repository,
+					image: repository?.owner.avatar_url || 'Not found',
+				},
+			};
+		})
+	);
+
+	return resolvedServices;
+}
+
+export async function getService(email: string | null | undefined, id: string): Promise<ServiceType | null> {
+	await db.connect();
+
+	if (!email) {
+		return null;
+	}
+
+	const role = await getRole(email);
+
+	let service;
+
+	if (role.includes('admin')) {
+		service = await Service.findOne<IService>({ _id: id }, { _id: 0 });
+	} else {
+		const user = await User.findOne<IUser>({ email });
+
+		if (!user) {
+			return null;
+		}
+
+		service = await Service.findOne<IService>(
+			{
+				$or: [{ owner: user._id }, { users: user._id }],
+			},
+			{ _id: 0 }
+		);
+	}
+
+	if (!service) {
+		return null;
+	}
+
+	const owner = await User.findOne<IUser>({ _id: service.owner });
+
+	if (!service.repository) {
 		return {
-			id: service._id.toString(),
-			name: service.name,
-			status: service.status,
+			...service.toObject(),
+			owner: owner?.name,
+			id,
 		};
+	}
+
+	const response = await fetch(`https://api.github.com/repos/${service.repository.replace('https://github.com/', '')}`, {
+		headers: {
+			Authorization: `token ${process.env.GITHUB_TOKEN}`,
+		},
 	});
+	const repository = await response.json();
+
+	return {
+		...service.toObject(),
+		owner: owner?.name,
+		repository: {
+			url: service.repository,
+			image: repository?.owner?.avatar_url || '',
+		},
+		id,
+	};
 }
 
 export async function createService(data: { name: string; description: string; owner: string }): Promise<ErrorType> {
@@ -57,12 +140,76 @@ export async function createService(data: { name: string; description: string; o
 		name: data.name,
 		description: data.description,
 		owner: user,
+		repository: '',
 		users: [],
 	});
 
 	if (!service) {
 		return {
 			error: 'Service not created',
+			status: 500,
+		};
+	}
+
+	return {
+		error: '',
+		status: 200,
+	};
+}
+
+export async function updateService({ name, description, repository, id, email }: { name: string | undefined; description: string | undefined; repository: string | undefined; id: string; email: string }): Promise<ErrorType> {
+	await db.connect();
+
+	if (!email) {
+		return {
+			error: 'Unauthorized',
+			status: 401,
+		};
+	}
+
+	const role = await getRole(email);
+
+	let service;
+
+	if (role.includes('admin')) {
+		service = await Service.findOne<IService>({ _id: id });
+	} else {
+		const user = await User.findOne<IUser>({ email });
+
+		if (!user) {
+			return {
+				error: 'Unauthorized',
+				status: 401,
+			};
+		}
+
+		service = await Service.findOne<IService>({
+			$or: [{ owner: user._id }, { users: user._id }],
+		});
+	}
+
+	if (!service) {
+		return {
+			error: 'Service not found',
+			status: 404,
+		};
+	}
+
+	console.log('repository', repository); // undefined
+	console.log('service', service._id);
+
+	const updatedService = await Service.updateOne(
+		{ _id: service._id },
+		{
+			name: name || service.name,
+			description: description || service.description,
+			repository: repository ? repository.replace(/\/$/, '') : null,
+		}
+	);
+
+	if (!updatedService) {
+		return {
+			error: 'Service not updated',
 			status: 500,
 		};
 	}
