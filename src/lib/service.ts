25852e3a75bdd9a9dbfa7c42b84ad15a0a374f2f
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-19 21:29:51 +0200

    Refactor getService function to exclude unnecessary fields from the query

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -89,7 +89,7 @@ export async function getService(email: string | null | undefined, id: string): 
 				{
 					_id: { $in: service.dockers },
 				},
-				{ _id: 0 }
+				{ _id: 0, __v: 0, 'ports._id': 0, 'mounts._id': 0 } //
 			);
 		}
 
