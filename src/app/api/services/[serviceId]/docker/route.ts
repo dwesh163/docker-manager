db822215902cd7502f34316d7ba2a07e20291501
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-19 17:16:36 +0200

    Refactor Docker route to handle POST requests for creating a Docker container

---
+++ src/app/api/services/[serviceId]/docker/route.ts
@@ -0,0 +1,29 @@
+import { createDocker } from '@/lib/docker';
+import { getServerSession, Session } from 'next-auth';
+import { NextResponse } from 'next/server';
+
+interface Params {
+	serviceId: string;
+}
+
+export async function POST(req: Request, { params }: { params: Params }) {
+	const { name, image } = await req.json();
+
+	const session: Session | null = await getServerSession();
+
+	if (!session) {
+		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+	}
+
+	if (!image || !name) {
+		return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+	}
+
+	const docker = await createDocker({ name, image: image.includes(':') ? image : `${image}:latest`, serviceId: params.serviceId, owner: session.user.email || '' });
+
+	if (docker.error) {
+		return NextResponse.json({ error: docker.error }, { status: docker.status });
+	}
+
+	return NextResponse.json({ success: true });
+}
