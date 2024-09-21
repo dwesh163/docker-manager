213aebf45f0c558a6ae862bce4678f7ea626e6ef
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-21 12:42:44 +0200

    Refactor network.ts to exclude network property in query and add network type definition

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,28 +1,53 @@
 import { INetwork, Network } from '@/models/Network';
 import { IService, Service } from '@/models/Service';
+import { IDocker, Docker as DockerModel } from '@/models/Docker';
+import { NetwoksType } from '@/types/network';
 import Docker from 'dockerode';
 
 const docker = new Docker({ socketPath: '/var/run/docker.sock', version: 'v1.46' });
 
 export async function createNetwork({ name }: { name: string }): Promise<INetwork> {
 	try {
-		const existingNetwork = await Network.findOne({ name });
+		const existingNetwork: INetwork | null = await Network.findOne({ name });
 
-		if (existingNetwork) {
+		const dockerNetworks = await docker.listNetworks({ filters: { name: [name] } });
+		const existingDockerNetwork = dockerNetworks.find((n: any) => n.Name === name);
+
+		if (existingNetwork && !existingDockerNetwork) {
+			const network = await docker.createNetwork({ Name: name });
+			if (!network) {
+				throw new Error('Failed to create network in Docker');
+			}
+
+			console.log('Network created in Docker:', network);
 			return existingNetwork;
 		}
 
-		const network = await docker.createNetwork({ Name: name });
+		if (!existingNetwork && existingDockerNetwork) {
+			const createdNetwork = await Network.create({
+				id: existingDockerNetwork.Id,
+				name: existingDockerNetwork.Name,
+			});
 
-		if (!network) {
-			throw new Error('Network creation failed');
+			console.log('Network created in DB:', createdNetwork);
+			return createdNetwork;
 		}
 
-		const createdNetwork = await Network.create({ id: network.id, name });
+		if (!existingNetwork && !existingDockerNetwork) {
+			const network = await docker.createNetwork({ Name: name });
+			if (!network) {
+				throw new Error('Failed to create network');
+			}
 
-		return createdNetwork;
+			const createdNetwork = await Network.create({ id: network.id, name });
+			console.log('Network created in Docker and DB:', createdNetwork);
+
+			return createdNetwork;
+		}
+
+		return existingNetwork as INetwork;
 	} catch (error) {
-		console.error('Error creating network:', error);
+		console.error('Error creating or checking network:', error);
 		throw error;
 	}
 }
@@ -40,6 +65,28 @@ export async function getNetwork({ serviceId }: { serviceId: string }): Promise<
 		return network;
 	} catch (error) {
 		console.error('Error getting network:', error);
+		throw error;
+	}
+}
+
+export async function getNetworks(): Promise<NetwoksType[]> {
+	try {
+		const networks = await Network.find();
+
+		const dockers = await DockerModel.find();
+
+		const networksData = networks.map((network) => {
+			const dockersData = dockers.filter((docker) => docker.networks.includes(network._id.toString()));
+
+			return {
+				name: network.name,
+				dockers: dockersData.map((docker) => ({ name: docker.name, status: docker.currentStatus })),
+			};
+		});
+
+		return networksData;
+	} catch (error) {
+		console.error('Error getting networks:', error);
 		throw error;
 	}
 }
