312cd4fee3a39ae0fa3aad42c5189409e0500d72
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-21 21:15:07 +0200

    Refactor networks page to use NetworkVisualization component

--- src/app/(root)/networks/page.tsx
+++ src/app/(root)/networks/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/auth';
 import { Session } from 'next-auth';
 import { getNetworks } from '@/lib/network';
-import { NetworksCard } from '@/components/networks';
+import { NetworkVisualization } from '@/components/networks';
 
 export default async function NetworksPage() {
 	const session: Session | null = await getServerSession(authOptions);
@@ -12,7 +12,7 @@ export default async function NetworksPage() {
 
 	return (
 		<div className="flex flex-col items-center flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-			<NetworksCard networksData={networks} />
+			<NetworkVisualization networksData={networks} />
 		</div>
 	);
 }
